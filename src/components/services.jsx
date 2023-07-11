52215687d9904cf40abc8831d4b93ede2a8e8360
FabriMontserrat <fabrimontserrat@gmail.com>
2023-07-11 17:51:56 -0300

    Primer commit

---
+++ src/components/services.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Hydrocooling</h2>
+          <p>
+            El sistema Hydrocooling posee muchas ventajas en comparacion con el sistema de aire convencional
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
